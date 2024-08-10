3315b00201bdb47527433d456c218149991bb6c4
DeepaNandhini <nandhinideepa1998@gmail.com>
2024-08-10 18:42:55 +0530

    Added files

---
+++ src/pages/Advertisement.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Advertisement() {
+  return (
+    <div className="advertisement">
+        <hr/>
+      Submitted and coded by Deepa Nandhini . M, you may send mail to my email address which is Deepa1998@gmail.com.
+        <hr/>
+    </div>
+  );
+}
+
+export default Advertisement;
